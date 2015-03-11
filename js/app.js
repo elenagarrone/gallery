eb3959b2639947532fd04b33ebd984f67f059121
elena <elena.15@hotmail.it>
2015-03-11 15:57:32 +0000

    added fencybox

---
+++ js/app.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  $(".fancybox").fancybox({
+    prevEffect  : 'none',
+    nextEffect  : 'none',
+    helpers : {
+      title : {
+        type: 'outside'
+      }
+    }
+  });
+});
